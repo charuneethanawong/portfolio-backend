4f74d0b30036cbd682a7e2c4d929a7bfcfb94375
charuneethanawong <charunee.thana@gmail.com>
2023-08-19 03:36:32 +0700

    myproject

---
+++ index.js
@@ -0,0 +1,60 @@
+const express = require('express')
+const cors = require('cors')
+const app = express()
+const mysql = require('mysql')
+
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '123456',
+    database: 'portfolio',
+    
+})
+connection.connect((err)=>{
+    if(err){
+        console.log('error connecting =',err);
+        return
+    }
+    console.log('mysql successfully connected');
+})
+app.use(cors())
+app.use(express.json())
+
+app.get('/users',(req,res)=>{
+    connection.query("SELECT * FROM project",(err,result)=>{
+        if (err)console.log(err)
+        else{res.send(result)}
+})
+})
+
+app.post('/adddata',(req,res)=>{
+   const {firstName,lastName,age} = req.body
+    connection.query("INSERT INTO project (firstname, lastname, age) VALUES(?,?,?)",
+    [firstName,lastName,age]
+    ,(err,result)=>{
+        if (err)console.log(err)
+        else{res.send('data was added')}
+})
+})
+
+app.put('/updatedata/:id',(req,res)=>{
+    const id = req.params.id
+    const {firstName,lastName,age} = req.body
+    connection.query("UPDATE project SET firstname=?, lastname=?, age=? WHERE id =?",
+    [firstName,lastName,age,id]
+    ,(err,result)=>{
+        if (err)console.log(err)
+        else{res.send('updated successfully')}
+})
+})
+app.delete('/deletedata/:id',(req,res)=>{
+    const id = req.params.id
+    connection.query("DELETE FROM project WHERE id =?",
+    [id]
+    ,(err,result)=>{
+        if (err)console.log(err)
+        else{res.send('deleted successfully')}
+})
+})
+app.listen(3333,()=>{console.log('server is running 3333');})
